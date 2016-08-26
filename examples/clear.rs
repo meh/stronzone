0b5f55624ce780f8ff54a38712297abd46358cab
meh <meh@schizofreni.co>
2016-08-26 20:28:26 +0200

    examples/clear: support grid definition

--- examples/clear.rs
+++ examples/clear.rs
@@ -1,10 +1,27 @@
 extern crate stronzone;
 
+extern crate clap;
+use clap::{Arg, App};
+
 fn main() {
-	let mut canvas = stronzone::canvas()
-		.add((0, 0), stronzone::grid::new()
-			.brightness(0)
-			.connect(("192.168.0.145", 9001)).unwrap());
+	let matches = App::new("text")
+		.version("0.1.0")
+		.author("meh. <meh@schizofreni.co>")
+		.about("Clear and turn off all leds.")
+		.arg(Arg::with_name("grid")
+			.short("g")
+			.long("grid")
+			.takes_value(true)
+			.multiple(true)
+			.required(true)
+			.help("Add a grid to the canvas."))
+		.get_matches();
+
+	// Create the canvas based on definition.
+	let mut canvas = matches.values_of("grid").unwrap().fold(stronzone::canvas(), |canvas, def| {
+		let (at, grid) = stronzone::util::grid(def, 0);
+		canvas.add(at, grid)
+	});
 
 	canvas.clear();
 	canvas.sync().unwrap();
